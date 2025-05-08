edcadb8ef024615b994b83e38b61f3b907b25eda
RobinsonDominguez <robinsondominguezulloa349@gmail.com>
2025-05-08 11:55:52 -0500

    feat: :sparkles: Adding the first folders and files

---
+++ Dia3/Ejercicios/PrintsFuctions_HackerRank_DominguezRobinson.py
@@ -0,0 +1,14 @@
+################
+###### Dia3 ####
+################
+
+#ingreso de las varianles
+if __name__ == '__main__':
+    n = int(input())
+    num = ""
+    for i in range (1,n+1):
+        num = num+str(i) 
+        #mostrar los resultados
+    print(num)
+
+    #Creado por: Dominguez Ulloa Robinson         -      T.I: 1.104.128.994
